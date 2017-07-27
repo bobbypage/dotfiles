e30052c2bdc0e93b3dc9c6c7ad7cbf5695324b32
David Porter <david@porter.me>
2017-07-27 15:55:32 -0700

    vundle

--- install.sh
+++ install.sh
@@ -6,3 +6,10 @@ ln -sfv $HOME/dotfiles/.tmux.conf ~
 ln -sfv $HOME/dotfiles/.vimrc ~
 ln -sfv $HOME/dotfiles/.zshrc ~
 
+VUNDLE=$HOME/.vim/bundle/Vundle.vim
+
+if [ ! -d "$VUNDLE" ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+fi
+
+vim +PluginInstall +qall
