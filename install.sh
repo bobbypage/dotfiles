b3611c7488057d7ce1e507f97f4713fc994f9eda
David Porter <david@porter.me>
2017-07-27 15:41:03 -0700

    add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+ln -sfv $HOME/dotfiles/.aliases ~
+ln -sfv $HOME/dotfiles/.bash_profile ~
+ln -sfv $HOME/dotfiles/.gitconfig ~
+ln -sfv $HOME/dotfiles/.gitignore_global ~
+ln -sfv $HOME/dotfiles/.tmux.conf ~
+ln -sfv $HOME/dotfiles/.vimrc ~
+ln -sfv $HOME/dotfiles/.zshrc ~
+
